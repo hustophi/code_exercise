9d089ca2e74afe13f128172dc2d2ede01adef407
hustophi <51041892+hustophi@users.noreply.github.com>
2021-04-26 18:48:03 +0800

    Update Middleproblem-set.py

--- Middleproblem-set.py
+++ Middleproblem-set.py
@@ -30,8 +30,8 @@ class Solution:
 # @param str1 string字符串 the string
 # @param str2 string字符串 the string
 # @return string字符串
-#遍历其中一个字符串，maxlen记录当前已找到的最长长度，找以str[i]结尾长度为maxlen+1的公共子串，找到后，更新maxlen，ans
-#奥义：每次更新maxlen和ans后，长度更长(即maxlen+1)的公共子串只会以当前字符之后的字符为结尾，即按照我们的更新规则，当前字符前不会存在长度更长的公共子串
+#遍历其中一个字符串，maxlen记录当前已找到公共子串的最大长度，找长度为maxlen+1的第一个公共子串，找到后，更新maxlen，ans
+#奥义：每次更新maxlen和ans后，长度更长(即maxlen+1)的第一个公共子串只会以当前字符之后的字符为结尾，即按照我们的更新规则，当前字符前不会存在长度更长的公共子串
 #因此复杂度为O(len(str1))
 class Solution:
     def LCS(self , str1 , str2 ):
@@ -40,7 +40,7 @@ class Solution:
         maxlen = 0
         ans = ''
         for i in range(len(str1)):
-            if str1[i - maxlen:i + 1] in str2: #for遍历比in判断的复杂度可能低一些,因此在开始先判断字符串长度大小关系
+            if str1[i - maxlen:i + 1] in str2: #for遍历比in判断的复杂度可能低一些,因此在开始先判断字符串长度大小关系进行优化
                 ans = str1[i-maxlen:i+1]      #注意ans与maxlen的更新顺序不能颠倒
                 maxlen += 1
         return ans
