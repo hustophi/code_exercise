831f821a01cb1dc24ee8366f6611a1d27dab0507
hustophi <51041892+hustophi@users.noreply.github.com>
2021-11-07 18:11:53 +0800

    Update Middleproblem-set.py

--- Middleproblem-set.py
+++ Middleproblem-set.py
@@ -385,3 +385,25 @@ class Solution:
         return 0
         # write code here
 ###################################################
+#在一个二维数组array中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序
+#请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+# @param target int整型 
+# @param array int整型二维数组 
+# @return bool布尔型
+#
+class Solution:
+    def Find(self , target: int, array: List[List[int]]) -> bool:
+        end_row, start_col = len(array) - 1, 0
+        return FindHelp(target, array, end_row, start_col)
+def FindHelp(target, array, end_row, start_col):
+    #IMPORTANT: 基于array的特点,从左下角(记为ld)开始查找
+    #当target=ld,直接返回True; 当target>ld,只须在ld所在列右方查找; 当target<ld只需在ld所在行上方查找,递归
+    if start_col >= len(array[0]) or end_row < 0: return False    #超出范围还未找到,返回False
+    if target == array[end_row][start_col]: return True
+    if target > array[end_row][start_col]: 
+        return FindHelp(target, array, end_row, start_col + 1)
+    if target < array[end_row][start_col]:
+        return FindHelp(target, array, end_row - 1, start_col)
+        # write code here
