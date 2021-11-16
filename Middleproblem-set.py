e6a285084f947ecfb60bc7f9269b026ed61646f2
hustophi <51041892+hustophi@users.noreply.github.com>
2021-11-16 17:49:50 +0800

    Update Middleproblem-set.py

--- Middleproblem-set.py
+++ Middleproblem-set.py
@@ -2,7 +2,6 @@
 #空间复杂度 O(len(S))，时间复杂度 O(len(S)+len(T))
 #
 # 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-#
 # 计算模板串S在文本串T中出现了多少次
 # @param S string字符串 模板串
 # @param T string字符串 文本串
@@ -543,3 +542,25 @@ class Solution:
                 r = mid
         return l
         # write code here
+###################################################
+#给定无序数组arr，返回其中最长的连续序列的长度(要求值连续，位置可以不连续,例如 3,4,5,6为连续的自然数）
+#
+# max increasing subsequence
+# @param arr int整型一维数组 the array
+# @return int整型
+#
+class Solution:
+    def MLS(self , arr ):
+        long=0
+        arr=set(arr)
+        for num in arr:
+            if num-1 not in arr:
+                cur=num            #找到每一段连续子列的最小值
+                cur_long=1
+                while cur+1 in arr:       #计算该段连续子列的长度
+                    cur+=1
+                    cur_long+=1
+                    long=max(long, cur_long)
+        return long
+        # write code here
+
