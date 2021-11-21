ad76b6d6589c60c96c86a721ca6b46510b661b8e
hustophi <51041892+hustophi@users.noreply.github.com>
2021-11-21 13:24:02 +0800

    Update Hardproblem-set.py

--- Hardproblem-set.py
+++ Hardproblem-set.py
@@ -53,3 +53,36 @@ class Solution:
             res = max(first[i] + second[i], res)
         return res
         # write code here
+################################################
+#二叉树里面的路径被定义为:从该树的任意节点出发，经过父=>子或者子=>父的连接，达到任意节点的序列。
+#注意: 1.同一个节点在一条二叉树路径里中最多出现一次; 2.一条路径至少包含一个节点，且不一定经过根节点
+#给定一个二叉树的根节点root，请你计算它的最大路径和
+#key: 递归计算经过每个节点的最大路径和,维护一个最大值
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# @param root TreeNode类 
+# @return int整型
+#
+class Solution:
+    def maxPathSum(self , root ):
+        res = -float('inf')  #初始化
+        def help(root):      #IMPORTANT: help函数递归计算以该节点为根节点的子树中寻找以该节点为起点的一条路径,使得该路径上的节点值之和最大
+            nonlocal res     #维护一个全局变量, 记录最大值
+            if root:
+                l = help(root.left)
+                r = help(root.right)
+                val = max(l, 0) + max(r, 0) + root.val  #val为经过该节点的最大路径和 (NOTES:此步可直接在help递归过程中得到,而无需在help外单独使用递归,可减少空间复杂度)
+                if val > res:
+                    res = val 
+                return max(l, r, 0) + root.val
+            else:
+                return 0
+        help(root)
+        return res 
+        # write code here        
+
