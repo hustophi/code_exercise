6c890cace66dff8a94ca8bbb7e073d0ebe45b235
hustophi <51041892+hustophi@users.noreply.github.com>
2023-10-28 13:20:10 +0800

    Update Hardproblem-set.py

--- Hardproblem-set.py
+++ Hardproblem-set.py
@@ -405,3 +405,29 @@ class Solution:
                         dp[i][j] = dp[i][j-2]
         return dp[-1][-1]
         # write code here
+################################################
+#给定一个二维数组map，含义是一张地图
+#游戏的规则如下:
+#1）骑士从左上角出发，每次只能向右或向下走，最后到达右下角见到公主。
+#2）地图中每个位置的值代表骑士要遭遇的事情。如果是负数，说明此处有怪兽，要让骑士损失血量。如果是非负数，代表此处有血瓶，能让骑士回血。
+#3）骑士从左上角到右下角的过程中，走到任何一个位置时，血量都不能少于1。为了保证骑土能见到公主，初始血量至少是多少?
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可 
+# @param mp int整型二维数组 
+# @return int整型
+#dp[i][j]的含义是如果骑士从(i,j)出发最后走到右下角，骑士至少应该具备的血量
+#从右往左, 从下往上更新dp, 最终结果即为dp[0][0]
+class Solution:
+    def dnd(self , mp: List[List[int]]) -> int:
+        # write code here
+        dp = [[0] * len(mp[0]) for i in range(len(mp))]
+        dp[-1][-1] = max(1-mp[-1][-1], 1)
+        for i in range(len(mp)-2, -1, -1):
+            dp[i][-1] = max(dp[i+1][-1]-mp[i][-1], 1)
+        for j in range(len(mp[0])-2, -1, -1):
+            dp[-1][j] = max(dp[-1][j+1]-mp[-1][j], 1)
+        for i in range(len(mp)-2, -1, -1):
+            for j in range(len(mp[0])-2, -1, -1):
+                dp[i][j] = min(max(dp[i+1][j]-mp[i][j], 1), max(dp[i][j+1]-mp[i][j], 1))
+        return dp[0][0]
+        # write code here
